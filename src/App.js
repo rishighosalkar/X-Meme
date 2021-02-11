bc06822f546ae8a0dd34d70475d262e5de5f9166
Rishi <rushighosalkar@gmail.com>
2021-02-11 12:28:05 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import UserComponent from './components/UserComponent';
+import EditUserMeme from './components/EditUserMeme';
+import Navbar from './components/NavbarComponent';
+import UsersList from './components/UsersList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<UserComponent />
+    <Router>
+      <div className="container">
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={UsersList} />
+      <Route path="/edit/:id" exact component={EditUserMeme} />
+      <Route path="/user" exact component={UserComponent} />
+      </div>
+    </Router>
   );
 }
 
